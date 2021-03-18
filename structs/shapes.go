540931fdd28e5c5750e6c0e99070fd9fdc05f414
Paul Karayan <paulkarayan@gmail.com>
2021-03-18 12:53:16 -0700

    field naming

--- structs/shapes.go
+++ structs/shapes.go
@@ -19,14 +19,19 @@ func (r Rectangle) Perimeter() float64 {
 	return 2 * (r.Width + r.Height)
 }
 
-// func Area(rectangle Rectangle) float64 {
-// 	return rectangle.Width * rectangle.Height
-// }
-
 type Circle struct {
 	Radius float64
 }
 
 func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
+}
+
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) * 0.5
 }
